932f7a68c96f88818d8060678c9e98f06dba7f06
sodam <sodamlee315@gmail.com>
2024-02-19 20:52:18 +0900

    jpa auditing으로 baseEntity 추가

---
+++ news-feed/src/main/java/com/school/newsfeed/common/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.school.newsfeed.common;
+
+import jakarta.persistence.Column;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+public abstract class BaseEntity extends BaseTimeEntity{
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}
