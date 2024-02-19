f32301bf114ab11aef6ab9c617a0ba90ea919795
sodam <sodamlee315@gmail.com>
2024-02-19 23:32:58 +0900

    SchoolSubscribe(엔티티, 학교구독)추가

---
+++ news-feed/src/main/java/com/school/newsfeed/domain/schoolsubscribe/SchoolSubscribe.java
@@ -0,0 +1,32 @@
+package com.school.newsfeed.domain.schoolsubscribe;
+
+import com.school.newsfeed.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class SchoolSubscribe extends BaseEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    @Column(name="school_subscribe_id",columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column
+    private UUID schoolId;
+
+    @Column
+    private UUID userId;
+
+    @Column
+    private Boolean del; //default=false
+}
