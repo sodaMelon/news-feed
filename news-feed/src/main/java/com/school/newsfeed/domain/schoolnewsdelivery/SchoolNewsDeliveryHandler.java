e4559e92e041eb75369d8dfd231ba42b149bf2e2
sodam <sodamlee315@gmail.com>
2024-02-21 21:02:56 +0900

    비동기 실행 제거(아직 검토가 필요함)

--- news-feed/src/main/java/com/school/newsfeed/domain/schoolnewsdelivery/SchoolNewsDeliveryHandler.java
+++ news-feed/src/main/java/com/school/newsfeed/domain/schoolnewsdelivery/SchoolNewsDeliveryHandler.java
@@ -3,6 +3,7 @@ package com.school.newsfeed.domain.schoolnewsdelivery;
 import com.school.newsfeed.domain.schoolsubscribe.SchoolSubscribe;
 import com.school.newsfeed.domain.schoolsubscribe.SchoolSubscribeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -19,10 +20,9 @@ public class SchoolNewsDeliveryHandler {
     private final SchoolNewsDeliveryRepository deliveryRepository;
 
     /**
-     * 학교_소식 생성 후, 실제 구독자들에게 학교_소식_배송 (구독자가 늘어날 수록 무거워지기때문에 비동기로 실행함)
+     * 학교_소식 생성 후, 실제 구독자들에게 학교_소식_배송 (구독자가 늘어날 수록 무거워지기때문에 비동기로 실행 검토 필요 )
      */
-    @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
+    @EventListener
     public void eventHandler(SchoolNewsDelivery deliveryEvent){
         List<SchoolSubscribe> activeSubscribes = subscribeRepository.findAllBySchoolIdAndDel(deliveryEvent.getSchoolId(),false);
         List<SchoolNewsDelivery> realDeliveries = new ArrayList<>();
